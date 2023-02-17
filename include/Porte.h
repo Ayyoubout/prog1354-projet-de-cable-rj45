94a9b9dda4f383b66f5d650fa692d167a8445710
Ayyoubout <Ayoubout83@gmail.com>
2023-02-16 23:55:57 -0400

    3eme commit

--- include/Porte.h
+++ include/Porte.h
@@ -26,4 +26,31 @@ public:
     maronBlan = MB;
     maron    = M;
     }
+        
+    //j'ai declarais cette fonction pour envoyer un signale avec les brouche de outPut 
+    void allume(){  
+        digitalWrite(vertBlan, HIGH);
+        digitalWrite(verte, HIGH);
+        digitalWrite(orangeBlan, HIGH);
+        digitalWrite(bleu, HIGH);
+        digitalWrite(bleuBlan, HIGH);
+        digitalWrite(orange, HIGH);
+        digitalWrite(maronBlan, HIGH);
+        digitalWrite(maron, HIGH);
+    }
+    //cette fonction pour essayer est ce que le caple ca marche ou ca marche pas 
+    // pour esssayer j'ai fais un tableau de 8 casier et j'ai rediger le contenu de chaque casier 
+    bool teste(){
+        int TabGauche[8] = {};
+        int somme = 0; 
+        TabGauche[0] = digitalRead(vertBlan);
+        TabGauche[1] = digitalRead(verte);
+        TabGauche[2] = digitalRead(orangeBlan);
+        TabGauche[3] = digitalRead(bleu);
+        TabGauche[4] = digitalRead(bleuBlan);
+        TabGauche[5] = digitalRead(orange);
+        TabGauche[6] = digitalRead(maronBlan);
+        TabGauche[7] = digitalRead(maron);
+        
+    }
 }
